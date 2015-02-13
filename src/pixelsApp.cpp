a604d7b6c20397854fb55a270e49b0372ad67e9f
Christopher Fonseka <chris@roli.com>
2015-02-13 10:15:24 +0000

    A fresh and promising approach

--- src/pixelsApp.cpp
+++ src/pixelsApp.cpp
@@ -8,75 +8,31 @@
 //--------------------------------------------------------------
 void pixelsApp::setup()
 {
-	camWidth = 320; // try to grab at this size.
-	camHeight = 240;
-	one.pos = ofxVec2f(0,0);
+  w = 320;
+  h = 240;
 
-	videoGrabber.setup(w, h);
-	
-	colorImg.allocate(camWidth,camHeight); //Image that will house the cameras output, used because of laziness
-	
-	colorImgHSV.allocate(camWidth,camHeight); //our HSB image that will house the color image and deal out the Hue, Saturation and brightness
-	
-	hueImg.allocate(camWidth,camHeight); //Hue map
-	satImg.allocate(camWidth,camHeight); //saturation map
-	briImg.allocate(camWidth,camHeight); //brightness map, not gonna be used but necessary
-	
-	reds.allocate(camWidth, camHeight); //our postRange image basically
-	
-	colorTrackedPixelsRed = new unsigned char [camWidth * camHeight]; //rangeImage
-	
-	trackedTextureRed.allocate(camWidth, camHeight, GL_LUMINANCE); //final output
-	
-	doDrawInfo = false;
-}	
+  anchoHue = anchoBri = anchoSat = 5;
+
+  verHsb = verSat = verHue = false;
+  verInput = true;
+
+  videoGrabber.setup(w, h);
+
+  //reserve memory for cv images
+  rgb.allocate(w, h);
+  hsb.allocate(w, h);
+  hue.allocate(w, h);
+  sat.allocate(w, h);
+  bri.allocate(w, h);
+  filtered.allocate(w, h);
+}
 
 //--------------------------------------------------------------
 void pixelsApp::update()
 {
 	if (videoGrabber.isFrameNew())
 	{
-		colorImgHSV = colorImg; //now we stuff the colorImg into our HSB image
-		colorImgHSV.convertRgbToHsv(); //now we convert the colorImg inside colorImgHSV into HSV
-		
-		colorImgHSV.convertToGrayscalePlanarImages(hueImg, satImg, briImg); //distribute the hue, saturation and brightness to hueImg, satImg, and briImg
-		
-		// As stated in the following discussion, due to a probable bug in ofxOpenCv addon,
-		// you have to explicitly call flagImageChanged() after convertToGrayscalePlanarImages().
-		// http://forum.openframeworks.cc/t/converttograyscaleplanarimages-problem/2693/0
-		hueImg.flagImageChanged();
-		satImg.flagImageChanged();
-		briImg.flagImageChanged();
-		
-		// ok cool, here we go:
-		unsigned char * huePixels = hueImg.getPixels(); //huePixels is now a raw array of pixels
-		unsigned char * satPixels = satImg.getPixels(); //satPixels is now a raw array of pixels just like huePixels
-		//unsigned char * briPixels = briImg.getPixels();
-		int nPixels = camWidth * camHeight; //get the number of pixels in the images since these raw images are continuous, so no breaks
-		//so pixel number camWidth + 1 would be the first pixels in the second row of pixels of the image
-		
-		/* huePixels is a gigantic black and white array. so every pixel has a value from 0 to 255. This represents the hue values from the original color image. Certain colors can
-		 be represented by certain hue ranges. hues from 4 to 21 are redish while 109 to 115 are green*/
-		
-		for (int i = 0; i < nPixels; i++){ //let's go through every pixel in hue pixels
-			if ((huePixels[i] >= one.hue - 12 && huePixels[i] <= one.hue + 12) && //if the hue is of a certain range
-				(satPixels[i] >= one.sat - 24 && satPixels[i] <= one.sat + 200)){ //if the saturation is of a certain range
-				colorTrackedPixelsRed[i] = 255; //mark this corresponding pixel white
-			} else {
-				colorTrackedPixelsRed[i] = 0; //if it doesn't fit then color the corresponding pixel black
-			}
-		}
-		
-		reds.setFromPixels(colorTrackedPixelsRed, camWidth, camHeight); //set reds from the colorTrackedPixelsRed array so it's all clean and openCv operable
-		finderRed.findContours(reds, 10,nPixels/3, 1, false, true); //lets find one (1) blob in the grayscale openCv image reds
-		
-		trackedTextureRed.loadData(colorTrackedPixelsRed, camWidth, camHeight, GL_LUMINANCE); //load up the data from the colorTrackedPixelsRed into a texture
-		
-		//------------------------------
-		
-		if(finderRed.blobs.size() > 0) {
-			one.pos = ofxVec2f(finderRed.blobs[0].centroid.x,finderRed.blobs[0].centroid.y); //if the blob exists, set it's associated color (one) to it's position
-		}
+
 	}
 }
 
@@ -84,47 +40,95 @@ void pixelsApp::update()
 //--------------------------------------------------------------
 void pixelsApp::draw()
 {
-	ofSetColor(255,255,255);
-	
 	if (videoGrabber.isFrameNew())
 	{
-		colorImg.setFromPixels(videoGrabber.getPixels());
+		rgb.setFromPixels(videoGrabber.getPixels());
 	}
-	
-	ofSetColor(0xffffff); //set a white color as the setColor
-	vidGrabber.draw(0,0); //draw our video for reference/viewing pleasure
-	colorImgHSV.draw(340, 0);
-	
-	trackedTextureRed.draw(20, 300); //draw everything that was found
-	ofDrawBitmapString("red",20, 280); //label
-	finderRed.draw(); //draw our contour tracker over the video
-	
-	glPushMatrix(); //start a new openGL stack
-	glTranslatef(20,300,0); //translate lower a bit
-	finderRed.draw(); //draw the contour tracker over the trackedTextureRed
-	glPopMatrix(); //end the stack
-	
-	if(finderRed.blobs.size() > 0) { //if the blob exists then state it's x and y
-		char tempStr1[255];
-		sprintf(tempStr1, "x : %f\ny : %f", finderRed.blobs[0].centroid.x, finderRed.blobs[0].centroid.y);
-		ofDrawBitmapString(tempStr1, 20, 250); //draw the string
-	}
+
+  ofSetColor(255,255,255);
+
+    //draw all cv images
+    if(verInput) rgb.draw(0,0);
+    if(verHsb) hsb.draw(0,0);
+    if(verHue) hue.draw(0,0);
+    if(verSat) sat.draw(0,0);
+    filtered.draw(w,0);
+    contours.draw(w,0);
+
+    ofSetColor(255, 0, 0);
+    ofFill();
+
+    //draw red circles for found blobs
+    for (int i=0; i<contours.nBlobs; i++) {
+        ofCircle(contours.blobs[i].centroid.x, contours.blobs[i].centroid.y, 20);
+    }
 }
 
 
-void testApp::mousePressed(int x, int y, int button){
-	
-	unsigned char * huePixels = hueImg.getPixels(); //teh hue
-	unsigned char * satPixels = satImg.getPixels(); //teh saturation
-	/*unsigned char * briPixels = briImg.getPixels(); //teh brightness*/ //unnecessary really, hue and sat should be enough
-	
-	x = MIN(x,hueImg.width-1); //find the smallest value out of those two so we don't crash if we click outside of the camera image
-	y = MIN(y,hueImg.height-1);
-	
-	if(button == 0) {
-		one.hue = huePixels[x+(y*hueImg.width)]; //set the hue
-		one.sat = satPixels[x+(y*satImg.width)]; //set the sat
-		/*one.bri = briPixels[x+(y*briImg.width)];*/
-	}
+void pixelsApp::mousePressed(int x, int y, int button)
+{
+  if(x<w && y<h){
+        //get hue value on mouse position
+        findHue = hue.getPixels()[y*w+x];
+        findSat = sat.getPixels()[y*w+x];
+        findBri = bri.getPixels()[y*w+x];
+    }
 }
 
+//--------------------------------------------------------------
+void pixelsApp::keyPressed(int key){
+  switch(key){
+        case '1':
+            verInput = true;
+            verHsb = verSat = verHue = false;
+            break;
+        case '2':
+            verHsb = true;
+            verInput = verSat = verHue = false;
+            break;
+        case '3':
+            verHue = true;
+            verInput = verSat = verHsb = false;
+            break;
+        case '4':
+            verSat = true;
+            verInput = verHue = verHsb = false;
+            break;
+    case 'a':
+      findHue = min(findHue + 1, 180);
+      break;
+    case 'z':
+      findHue = max(findHue - 1, 0);
+      break;
+    case 's':
+      findSat = min(findSat + 1, 100);
+      break;
+    case 'x':
+      findSat = max(findSat - 1, 0);
+      break;
+    case 'd':
+      findBri = min(findBri + 1, 100);
+      break;
+    case 'c':
+      findBri = max(findBri - 1, 0);
+      break;
+    case 'f':
+      anchoHue = min(anchoHue + 1, 180);
+      break;
+    case 'v':
+      anchoHue = max(anchoHue - 1, 0);
+      break;
+    case 'g':
+      anchoSat = min(anchoSat + 1, 100);
+      break;
+    case 'b':
+      anchoSat = max(anchoSat - 1, 0);
+      break;
+    case 'h':
+      anchoBri = min(anchoBri + 1, 100);
+      break;
+    case 'n':
+      anchoBri = max(anchoBri - 1, 0);
+      break;
+  }
+}
--- src/pixelsApp.h
+++ src/pixelsApp.h
@@ -3,18 +3,7 @@
 #include "ofMain.h"
 #include "ofAppEGLWindow.h"
 #include "VideoSource.h"
-//#include "ofxRPiCameraVideoGrabber.h"
 #include "ofxOpenCv.h"
-
-class color
-{
-public:
-	
-	float hue, sat;
-	
-	ofxVec2f pos;
-};
-
 
 class pixelsApp : public ofBaseApp {
 	public:
@@ -22,25 +11,14 @@ class pixelsApp : public ofBaseApp {
 		void setup();
 		void update();
 		void draw();
+		void keyPressed(int key);
 		void mousePressed(int x, int y, int button);
 
 	PiCameraSource videoGrabber;
-	
-	ofxCvColorImage colorImg; //Main color image which is gonna get wrapped
-	ofxCvColorImage colorImgHSV; //the image doing the wrapping
-	
-	ofxCvGrayscaleImage hueImg; //Hue map
-	ofxCvGrayscaleImage satImg; //Saturation map
-	ofxCvGrayscaleImage briImg; //Brightness map
-	
-	ofxCvGrayscaleImage reds; //Grayscale image we are gonna run the contour finder over to find our color
-	
-	color one; //color that we're gonna track
-	
-	unsigned char * colorTrackedPixelsRed; //just some raw images which we are gonna put pixels into
-	ofTexture trackedTextureRed; //color texture that we are gonna draw to
-	
-	ofxCvContourFinder finderRed; //contour finder, very handy
-	int						camWidth, camHeight;
-};
+	ofxCvColorImage rgb,hsb;
+	ofxCvGrayscaleImage hue,sat,bri,filtered;
+	ofxCvContourFinder contours;
 
+	int w,h;
+	int findHue, findSat, findBri, anchoHue, anchoSat, anchoBri;
+};
